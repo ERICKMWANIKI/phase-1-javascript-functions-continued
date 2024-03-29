22114d223102091bd3cc68f1f912afe3e6f031b0
Erick <EMWANIKINDIRANGU@gmail.com>
2024-03-29 20:09:43 +0300

    modified

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(Hobby = "roller-skate") {
+    return `This Saturday, I want to ${Hobby}!`;
+}
+
+let mondayWork =
+function(Work = "go to the office") {
+    return `This Monday, I will ${Work}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+}
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction()); 
+console.log(encouragingPromptFunction("amazing"));
